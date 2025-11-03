7fa587c4a58a43ddcc5742b066cf33892bed1142
Andebugulin <gulin.andrey2005@gmail.com>
2025-11-03 14:21:40 +0200

    mention that the project is open source in info section

--- app/src/main/java/com/example/screentimetracker/InfoActivity.kt
+++ app/src/main/java/com/example/screentimetracker/InfoActivity.kt
@@ -3,6 +3,9 @@ package com.example.screentimetracker // Use your actual package name
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar // For the top bar
+import android.content.Intent
+import android.net.Uri
+import android.widget.TextView
 
 class InfoActivity : AppCompatActivity() {
 
@@ -17,6 +20,13 @@ class InfoActivity : AppCompatActivity() {
         // Enable the Up button (back arrow)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = "About Awareen" // Set a title for the screen
+
+        val githubRepoLink = findViewById<TextView>(R.id.githubRepoLink)
+        githubRepoLink.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://github.com/Andebugulin/Awareen")
+            startActivity(intent)
+        }
     }
 
     // Handle the Up button press (back arrow)
