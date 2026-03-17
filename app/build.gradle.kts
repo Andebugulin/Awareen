0e65250dee4ac9fa94eb7ed9035073998ce96c86
Andebugulin <gulin.andrey2005@gmail.com>
2026-03-17 21:32:12 +0200

    v1.19

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.andebugulin.awareen2"
         minSdk = 26
         targetSdk = 35
-        versionCode = 20
-        versionName = "1.18"
+        versionCode = 21
+        versionName = "1.19"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
